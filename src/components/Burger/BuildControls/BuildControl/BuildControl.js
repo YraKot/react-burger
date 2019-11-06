d25ec8a4ae616182198d400152e512362928e08c
Yra Kot <yrakot@Yras-MacBook-Air.local>
2019-11-06 15:08:29 +0200

    add middleware

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -6,11 +6,12 @@ const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
         <button 
-            disabled={props.disabled}
-            onClick={props.remove} 
-            className={classes.Less}
-            >Less</button>
-        <button onClick={props.added} className={classes.More}>More</button>
+            className={classes.Less} 
+            onClick={props.removed} 
+            disabled={props.disabled}>Less</button>
+        <button 
+            className={classes.More} 
+            onClick={props.added}>More</button>
     </div>
 );
 
